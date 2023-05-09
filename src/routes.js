76ba65d42bcc41188236efdfaeb9abf33ffd603b
Marcos Aurelio <77805407+marcosaureliob@users.noreply.github.com>
2023-05-08 21:32:07 -0300

    Fim do projeto

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,29 @@
+import PaginaPadrao from "componentes/PaginaPadrao";
+import Rodape from "componentes/Rodape";
+import ScrollToTop from "componentes/ScrollToTop";
+import NaoEncontrada from "paginas/NaoEncontrada";
+import Post from "paginas/Post";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "./componentes/Menu";
-import Inicio from "./paginas/Inicio";
-import SobreMim from "./paginas/SobreMim";
-import Rodape from "componentes/Rodape";
-import PaginaPadrao from "componentes/PaginaPadrao";
-import Post from "paginas/Post";
-import NaoEncontrada from "paginas/NaoEncontrada";
+import Inicio from './paginas/Inicio';
+import SobreMim from './paginas/SobreMim';
 
 function AppRoutes() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Menu />
 
       <Routes>
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Inicio />} />
           <Route path="sobremim" element={<SobreMim />} />
-          <Route path="posts/:id" element={<Post/>}/>
         </Route>
+        
+        <Route path="posts/:id/*" element={<Post />} />
         <Route path="*" element={<NaoEncontrada />} />
       </Routes>
+
       <Rodape />
     </BrowserRouter>
   );
